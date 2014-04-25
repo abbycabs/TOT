54d1add12605dd6d0ce03cb51e6a8a0f3719dbff
Shane Wilson <shanezilla@gmail.com>
2014-04-25 10:26:36 -0400

    more stuff to get heroku to work

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :description "Tips on the TV/Toilet/Twitter"
   :url "https://github.com/acabunoc/TOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-jetty-adapter "1.2.0"]
+                 [ring/ring-jetty-adapter "1.2.2"]
                  [clj-time "0.7.0"]
                  [clj-http "0.9.1"]
                  [compojure "1.1.6"]
                  [liberator "0.11.0"]
+                 [ring/ring-servlet "1.2.2"]
                  [environ "0.5.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
